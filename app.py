1358205465437b11b311e8f32f5544c28fdae35a
MrRomacka <romackad@gmail.com>
2021-04-10 16:53:42 +0500

    First half-working

--- app.py
+++ app.py
@@ -4,12 +4,37 @@ import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
-def full(pos):
-    return
+def full():
+    db_connection = sqlite3.connect('db/marks.db')
+    cur = db_connection.cursor()
+    exercises = cur.execute('SELECT task_id FROM Task').fetchall()
+    numerous = [list(ex)[0] for ex in exercises]
+    max_st = int(list(cur.execute('SELECT MAX(stu_id) FROM Student').fetchone())[0])
+    data = []
+    for j in range(1, max_st + 1):
+        name = list(cur.execute(f'SELECT stu_surname, stu_name, stu_group FROM Student WHERE stu_id = {int(j)}').fetchall()[0])
+        name = ' '.join(name)
+        data.append([])
+        data[j-1].append([name, j, 1])
+        for i in range(1, max(numerous) + 1):
+            exercises = cur.execute(f'SELECT st_task_id FROM StudentTask WHERE st_stu_id = {j}').fetchall()
+            exes = [list(ex)[0] for ex in exercises]
+            if i in exes:
+                mark = list((cur.execute(f'SELECT st_mark FROM StudentTask WHERE st_stu_id = {j} AND st_task_id = {i}').fetchone()))[0]
+                m_m = list(cur.execute(f'SELECT task_max_mark FROM Task WHERE task_id = {i}').fetchone())[0]
+                data[j-1].append([mark, m_m, 0])
+            else:
+                m_m = list((cur.execute(f'SELECT task_max_mark FROM Task WHERE task_id = {i}').fetchone()))[0]
+                data[j-1].append([0, m_m, 0])
+    return render_template('tb_2.html', data = data, numerous = numerous)
 
-@app.route('/tasks')
-def Tasky():
-    return
+@app.route('/task/<num>')
+def Tasky(num):
+    db_connection = sqlite3.connect('db/marks.db')
+    cur = db_connection.cursor()
+    inf = list(cur.execute(f'SELECT task_number, task_theme, task_equation, task_max_mark, task_date FROM Task WHERE task_id = {num}').fetchone())
+    print(inf)
+    return render_template('task.html', number = inf[0], theme = inf[1], eq = inf[2], maxi = inf[3], date = inf[4])
 
 @app.route('/theory')
 def Theoritic():
@@ -23,7 +48,7 @@ def pupil(pos):
     print(name)
     name = ' '.join(name)
     m_ex = int(list(cur.execute('SELECT MAX(task_id) FROM Task').fetchone())[0])
-    exercises = cur.execute('SELECT st_task_id FROM StudentTask WHERE st_stu_id = 3').fetchall()
+    exercises = cur.execute(f'SELECT st_task_id FROM StudentTask WHERE st_stu_id = {pos}').fetchall()
     exes = [list(ex)[0] for ex in exercises]
     data = []
     for i in range(1, m_ex + 1):
@@ -34,6 +59,8 @@ def pupil(pos):
         else:
             m_m = list((cur.execute(f'SELECT task_max_mark FROM Task WHERE task_id = {i}').fetchone()))[0]
             data.append([i, 0, m_m])
+    cur.close()
+    db_connection.close()
     return render_template('solo.html', data = data, name = name)
 
 
